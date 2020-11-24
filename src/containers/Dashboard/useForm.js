ee847fea208ba76b9537ed4283e32c4846d2b0a8
Amit Somani <somania1@uci.edu>
2020-11-23 16:35:11 -0800

    Removing cors header

--- src/containers/Dashboard/useForm.js
+++ src/containers/Dashboard/useForm.js
@@ -50,9 +50,6 @@ export default () => {
       
       let axiosConfig = {
         headers: {
-            'Access-Control-Allow-Origin' : '*',
-            'Access-Control-Allow-Methods':  "GET,HEAD,OPTIONS,POST,PUT",
-            'Access-Control-Allow-Headers' : "Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Authorization",
             'Authorization': data.signInUserSession.idToken.jwtToken,
         }
       };
